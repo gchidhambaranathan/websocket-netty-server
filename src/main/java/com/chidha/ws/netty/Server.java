0ec136fe791641b78c7eb94b9f8765c7a22465cb
chidhambaranathan G <chidhambaranathan.g@thoughtworks.com>
2022-03-21 17:18:41 +0530

    Initial commit

---
+++ src/main/java/com/chidha/ws/netty/Server.java
@@ -0,0 +1,38 @@
+package com.chidha.ws.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class Server {
+    private static final int PORT = 9000;
+    private static final String HOST = "192.168.0.101";
+
+    public static void main(String[] args) {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(5);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpInitializer());
+
+            Channel ch = b.bind(HOST,PORT).sync().channel();
+
+            ch.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
